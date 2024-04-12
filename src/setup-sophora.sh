cbf80ea94e37492d46c5dd15dcf4f5c022d03fef
Babilinx <babilinx.evx1o@simplelogin.com>
2024-04-12 17:56:16 +0200

    feat(sophora-install): add default structure and welcome message

--- src/setup-sophora.sh
+++ src/setup-sophora.sh
@@ -11,3 +11,72 @@
 source /usr/bin/gettext.sh
 export TEXTDOMAIN="setup-sophora"
 
+
+page=0
+
+
+# Credit: https://www.cyberciti.biz/faq/repeat-a-character-in-bash-script-under-linux-unix/
+repeat(){
+	local start=1
+	local end=${1:-80}
+	local str="${2:-=}"
+	local range=$(seq $start $end)
+	for i in $range ; do echo -n "${str}"; done
+}
+
+
+exit_menu() {
+    exit_box 2> /tmp/dialog_input
+    case `cat /tmp/dialog_input` in
+        1) echo "reboot";;
+        2) echo "exit";;
+        3) echo "poweroff";;
+    esac
+    
+    exit
+}
+
+
+exit_box() {
+    local title=`eval_gettext "Exit install"`
+    local menu_msg=`eval_gettext "Select what to do"`
+    local reboot_msg=`eval_gettext "Reboot computer"`
+    local exit_msg=`eval_gettext "Exit setup-sophora"`
+    local poweroff_msg=`eval_gettext "Poweroff computer"`
+    
+    dialog --title "$title" --no-cancel --menu "\n$menu_msg" 50 50 10 1 \
+    "$reboot_msg" 2 "$exit_msg" 3 "$poweroff_msg"
+}
+
+
+welcome_box() {
+    local title=`eval_gettext "Welcome to Sophora Linux installer"`
+    local text=`eval_gettext "This script will let you configure and install \
+Sophora Linux. Please be careful as you can delete important data!
+
+Sophora Linux team will not take the responsibility if you lose data!"`
+    
+    dialog --title "$title" --no-label "Exit" --yes-label "Continue" --yesno "\n$text" 50 50
+}
+
+
+main() {
+    case $page in
+        0) welcome_box && page=$((page+1)) || exit_menu;;
+        
+        *) exit;;
+    esac
+}
+
+
+
+
+if [ "$EUID" -ne 0 ]; then
+    eval_gettext "This program needs root rights!"; echo
+    #exit 1
+fi
+
+
+while true; do
+    main
+done
