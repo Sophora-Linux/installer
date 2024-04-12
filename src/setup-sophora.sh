7ef653e1807e5eb82015b69351ec667891880710
Bastien <bastien@alpine.my.domain>
2024-04-12 17:49:48 +0200

    feat(setup-sophora): create setup-sophora.sh

---
+++ src/setup-sophora.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# setup-sophora
+# Copyright (C) 2022 Wamuu Sudo (Orchid Linux)
+#               2022 Yannick Defais [Chevek] (Orchid Linux)
+#               2022 Crystal (Orchid Linux)
+#               2022 Babilinx (Orchid Linux)
+#               2024 Babilinx <babilinx.evx1o@simplelogin.com> (Sophora Linux)
+# This program is distributed under the terms of the GNU General Public License
+# version 3. See <https://gnu.org/licenses/> for more.
+
+source /usr/bin/gettext.sh
+export TEXTDOMAIN="setup-sophora"
+
